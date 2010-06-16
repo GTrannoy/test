85f6c6f2046ca542385b5038af8e2498321fc7ca
penacoba <penacoba@7f0067c9-7624-46c7-bd39-3fb5400c0213>
2010-06-16 14:27:07 +0000

    Waves file added

--- trunk/software/cadence_IUS/sim.tcl
+++ trunk/software/cadence_IUS/sim.tcl
@@ -1,21 +1,21 @@
-##ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work proasic3 ../src/proasic3_library/proasic3.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_package.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/reset_logic.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_crc.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_tx.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_rx.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_rx_osc.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/deglitcher.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_tx_rx.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_engine_control.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/DualClkRAM.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/dpblockram_clka_rd_clkb_wr_syn.vhd
-##ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/dpblockram_clka_rd_clkb_wr.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_consumed_vars.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_produced_vars.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/status_gen.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_dec_m_ids.vhd
-#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/nanofip.vhd
+#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work proasic3 ../src/proasic3_library/proasic3.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_package.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/reset_logic.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_crc.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_tx.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_rx.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_rx_osc.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/deglitcher.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_tx_rx.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_engine_control.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/DualClkRAM.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/dpblockram_clka_rd_clkb_wr_syn.vhd
+#ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/dpblockram_clka_rd_clkb_wr.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_consumed_vars.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_produced_vars.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/status_gen.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_dec_m_ids.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/nanofip.vhd
 
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/encounter.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/board_settings.vhd
@@ -35,4 +35,4 @@ ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work wor
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/nanofip_tb.vhd
 
 ncelab -nocopyright -nolog -messages -access +wc -messages -v93 -cdslib ./cds.lib -work worklib worklib.nanofip_tb:archi
-ncsim -gui -cdslib ./cds.lib -nocopyright -nolog -nokey worklib.nanofip_tb:archi -input waves.tcl
+#ncsim -gui -cdslib ./cds.lib -nocopyright -nolog -nokey worklib.nanofip_tb:archi -input waves.tcl
---
+++ trunk/software/cadence_IUS/waves.tcl
@@ -0,0 +1,49 @@
+probe -create -shm -waveform :fieldrive:clk
+probe -create -shm -waveform :fieldrive:reset
+
+probe -create -shm -waveform :fieldrive:launch_fip_transmit
+
+probe -create -shm -waveform :fieldrive:rx_block:feeder:fstate
+probe -create -shm -waveform :fieldrive:rx_block:feeder:fss_start
+probe -create -shm -waveform :fieldrive:rx_block:feeder:fss_complete
+probe -create -shm -waveform :fieldrive:rx_block:feeder:fes_start
+probe -create -shm -waveform :fieldrive:rx_block:feeder:fes_complete
+
+probe -create -shm -waveform :fieldrive:rx_block:msg_start
+probe -create -shm -waveform :fieldrive:rx_block:feeder:msg_start_dly
+probe -create -shm -waveform :fieldrive:rx_block:msg_complete
+
+probe -create -shm -waveform :fieldrive:rx_block:msg_block:mstate
+probe -create -shm -waveform :fieldrive:rx_block:mx
+
+probe -create -shm -waveform :fieldrive:rx_block:crc_gen_start
+probe -create -shm -waveform :fieldrive:rx_block:crc_gen_end
+
+probe -create -shm -waveform :fieldrive:rx_block:fcs
+probe -create -shm -waveform :fieldrive:rx_block:fcs_ready
+probe -create -shm -waveform :fieldrive:rx_block:fcs_valid
+probe -create -shm -waveform :fieldrive:rx_block:fcs_complete
+
+probe -create -shm -waveform :fieldrive:rx_block:dx_en
+probe -create -shm -waveform :fieldrive:rx_block:mux_select
+probe -create -shm -waveform :fieldrive:rx_block:mx_final
+probe -create -shm -waveform :fieldrive:rx_block:fx
+probe -create -shm -waveform :fieldrive:rx_block:dx_final
+
+probe -create -shm -waveform :fx_rxa
+probe -create -shm -waveform :fx_rxd
+
+probe -create -shm -waveform :fd_wdgn
+probe -create -shm -waveform :fd_txer
+
+probe -create -shm -waveform :fd_rstn
+probe -create -shm -waveform :fd_txck
+probe -create -shm -waveform :fx_txd
+probe -create -shm -waveform :fd_txena
+
+probe -create -shm -waveform :uclk
+probe -create -shm -waveform :urstin
+probe -create -shm -waveform :fieldrive:clk
+
+
+run 400 us
