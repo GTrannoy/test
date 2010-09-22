d1a5d9fc8481bf1b56023f2281e8c21fc903b4f5
penacoba <penacoba@7f0067c9-7624-46c7-bd39-3fb5400c0213>
2010-09-22 15:20:40 +0000

    Text files for board settings configuration and bus arbitrer scheduler added.

--- software/cadence_IUS/sim.tcl
+++ software/cadence_IUS/sim.tcl
@@ -1,23 +1,22 @@
 #ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work proasic3 ../src/proasic3_library/proasic3.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_package.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/reset_logic.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/DualClkRAM.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_DualClkRAM_clka_rd_clkb_wr.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_produced_vars.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_reset_unit.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_status_bytes_gen.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_crc.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_tx.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_rx.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_rx_osc.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/deglitcher.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_rx_tx_osc.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_rx_deglitcher.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_tx_rx.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_engine_control.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/DualClkRAM.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/dpblockram_clka_rd_clkb_wr_syn.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_model_constr_decoder.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_consumed_vars.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_produced_vars.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/status_gen.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_dec_m_ids.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/wf_engine_control.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/design/nanofip.vhd
 
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/encounter.vhd
-ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/board_settings.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/user_sequencer.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/wishbone_interface.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/user_interface.vhd
@@ -39,6 +38,7 @@ ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work wor
 
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/bus_arbitrer.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/fieldrive_interface.vhd
+ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/board_settings.vhd
 ncvhdl -nocopyright -nolog -messages -linedebug -v93 -cdslib ./cds.lib -work worklib ../src/tb_files/nanofip_tb.vhd
 
 ncelab -nocopyright -nolog -messages -access +wc -messages -v93 -cdslib ./cds.lib -work worklib worklib.nanofip_tb:archi
--- software/cadence_IUS/waves.tcl
+++ software/cadence_IUS/waves.tcl
@@ -1,7 +1,12 @@
 probe -create -shm -waveform :fieldrive:clk
 probe -create -shm -waveform :fieldrive:reset
 
-probe -create -shm -waveform :fieldrive:launch_fip_cycle
+#probe -create -shm -waveform :board:read_config_trigger
+#probe -create -shm -waveform :board:report_config_trigger
+#probe -create -shm -waveform :board:c_id_o
+
+probe -create -shm -waveform :fieldrive:fip_frame_trigger
+probe -create -shm -waveform :fieldrive:id_rp
 
 probe -create -shm -waveform :fieldrive:rx_block:feeder:fstate
 probe -create -shm -waveform :fieldrive:rx_block:feeder:fss_start
@@ -10,10 +15,19 @@ probe -create -shm -waveform :fieldrive:rx_block:feeder:fes_start
 probe -create -shm -waveform :fieldrive:rx_block:feeder:fes_complete
 
 probe -create -shm -waveform :fieldrive:rx_block:msg_start
-probe -create -shm -waveform :fieldrive:rx_block:feeder:msg_start_dly
+#probe -create -shm -waveform :fieldrive:rx_block:feeder:msg_start_dly
 probe -create -shm -waveform :fieldrive:rx_block:msg_complete
 
+probe -create -shm -waveform :fieldrive:rx_block:msg_block:msg_new_data_req
+probe -create -shm -waveform :fieldrive:rx_block:msg_block:en_count
+probe -create -shm -waveform :fieldrive:rx_block:msg_block:reset_count
+probe -create -shm -waveform :fieldrive:rx_block:msg_block:start_value
+probe -create -shm -waveform :fieldrive:rx_block:msg_block:count
+probe -create -shm -waveform :fieldrive:rx_block:msg_block:count_done
+
 probe -create -shm -waveform :fieldrive:rx_block:msg_block:mstate
+probe -create -shm -waveform :fieldrive:rx_block:msg_data
+probe -create -shm -waveform :fieldrive:rx_block:msg_go
 probe -create -shm -waveform :fieldrive:rx_block:mx
 
 probe -create -shm -waveform :fieldrive:rx_block:crc_gen_start
@@ -21,7 +35,7 @@ probe -create -shm -waveform :fieldrive:rx_block:crc_gen_end
 
 probe -create -shm -waveform :fieldrive:rx_block:fcs
 probe -create -shm -waveform :fieldrive:rx_block:fcs_ready
-probe -create -shm -waveform :fieldrive:rx_block:fcs_valid
+#probe -create -shm -waveform :fieldrive:rx_block:fcs_valid
 probe -create -shm -waveform :fieldrive:rx_block:fcs_complete
 
 probe -create -shm -waveform :fieldrive:rx_block:dx_en
@@ -46,19 +60,19 @@ probe -create -shm -waveform :fieldrive:tx_block:chopper:enable_chopping
 probe -create -shm -waveform :fieldrive:tx_block:chopper:chop_byte
 #probe -create -shm -waveform :fieldrive:tx_block:chopper:count_done
 probe -create -shm -waveform :fieldrive:tx_block:chopper:byte_nb
-probe -create -shm -waveform :fieldrive:tx_block:chopper:bytes_received
-probe -create -shm -waveform :fieldrive:tx_block:chopper:current_byte
+#probe -create -shm -waveform :fieldrive:tx_block:chopper:bytes_total
+#probe -create -shm -waveform :fieldrive:tx_block:chopper:current_byte
 probe -create -shm -waveform :fieldrive:tx_block:chopper:frame_data
-probe -create -shm -waveform :fieldrive:tx_block:chopper:pdu_type_byte
-probe -create -shm -waveform :fieldrive:tx_block:chopper:length_byte
-probe -create -shm -waveform :fieldrive:tx_block:chopper:control_byte
+#probe -create -shm -waveform :fieldrive:tx_block:chopper:pdu_type_byte
+#probe -create -shm -waveform :fieldrive:tx_block:chopper:length_byte
+#probe -create -shm -waveform :fieldrive:tx_block:chopper:control_byte
 
 #probe -create -shm -waveform :fieldrive:tx_block:chopper:control_ok
 probe -create -shm -waveform :fieldrive:tx_block:chopper:length_ok
 #probe -create -shm -waveform :fieldrive:tx_block:chopper:struct_ok
 probe -create -shm -waveform :fieldrive:tx_block:chopper:struct_check
-#probe -create -shm -waveform :fieldrive:tx_block:chopper:frame_struct_check
-#probe -create -shm -waveform :fieldrive:tx_block:chopper:frame_struct_ok
+probe -create -shm -waveform :fieldrive:tx_block:chopper:frame_struct_check
+probe -create -shm -waveform :fieldrive:tx_block:chopper:frame_struct_ok
 
 probe -create -shm -waveform :fx_rxa
 probe -create -shm -waveform :fx_rxd
@@ -74,7 +88,7 @@ probe -create -shm -waveform :uclk
 probe -create -shm -waveform :urst_from_nf
 probe -create -shm -waveform :urst_to_nf
 probe -create -shm -waveform :var1_rdy
-#probe -create -shm -waveform :var1_acc
+probe -create -shm -waveform :var1_acc
 probe -create -shm -waveform :var2_rdy
 #probe -create -shm -waveform :var2_acc
 probe -create -shm -waveform :var3_rdy
@@ -88,9 +102,11 @@ probe -create -shm -waveform :we
 probe -create -shm -waveform :adr
 probe -create -shm -waveform :dat_from_fip
 probe -create -shm -waveform :dat_to_fip
+#probe -create -shm -waveform :dat_i
+#probe -create -shm -waveform :dat_o
 probe -create -shm -waveform :user_logic:wb_interface:valid_bus_cycle
 probe -create -shm -waveform :user_logic:wb_interface:wb_state
 
-probe -create -shm -waveform :dut:uwf_produced_vars:s_byte
 
-run 1000 us
+run 3000 us
+
