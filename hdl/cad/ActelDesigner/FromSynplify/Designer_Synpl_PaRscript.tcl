2329d15dbdc2e52ef840beaf09687538ff5050e0
egousiou <egousiou@7f0067c9-7624-46c7-bd39-3fb5400c0213>
2011-12-15 20:08:12 +0000

    version of large scale PSI tests; also added nanoFIP version without TMR

--- hdl/cad/ActelDesigner/FromSynplify/Designer_Synpl_PaRscript.tcl
+++ hdl/cad/ActelDesigner/FromSynplify/Designer_Synpl_PaRscript.tcl
@@ -3,14 +3,14 @@ new_design -name "nanoFIP"  -family "ProASIC3"
 
 set_device -die "A3P400" -speed "STD" -package "208 PQFP" -speed "STD" -voltage "1.5" -iostd "LVTTL" -temprange "COM" -voltrange "COM"
 
-import_source -merge_timing "yes" -format "EDIF" -edif_flavor "GENERIC" {C:\ohr\cern-fip\trunk\hdl\cad\Synplify\nanofip.edn} -format "SDC" {C:\ohr\cern-fip\trunk\hdl\cad\Actel_designer\FromSynplify\Designer_Synpl_TimeConstr.sdc}
+import_source -merge_timing "yes" -format "EDIF" -edif_flavor "GENERIC" {C:\ohr\cern-fip\trunk\hdl\cad\Synplify\nanofip.edn} -format "SDC" {C:\ohr\cern-fip\trunk\hdl\cad\ActelDesigner\FromSynplify\Designer_Synpl_TimeConstr.sdc}
 
 compile
 
 create_clock -name {uclk_i} -period 25 uclk_i
 create_clock -name {wclk_i} -period 25 wclk_i
 
-import_aux -format "PDC" {C:\ohr\cern-fip\trunk\hdl\cad\libero\NanoFip\constraint\Designer_Synpl_Pinout.pdc}
+import_aux -format "PDC" {C:\ohr\cern-fip\trunk\hdl\cad\libero\nanoFip\constraint\Designer_Synpl_Pinout.pdc}
 
 layout -incremental "OFF"
 
